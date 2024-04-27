45598c0ae2b56b3880f50807d88b4bb9ec21cc32
pompodar <pompodar@gmail>
2024-04-27 19:30:23 +0300

    refactor

--- background.js
+++ background.js
@@ -1,49 +1,46 @@
+let timer;
+
+const getSecondsFromTime = (timeString) => {
+  const timeParts = timeString.split(":");
+  const hours = parseInt(timeParts[0]);
+  const minutes = parseInt(timeParts[1]);
+  const seconds = parseInt(timeParts[2]);
+  return hours * 3600 + minutes * 60 + seconds;
+};
+
+const playBookmark = (videoKey, bookmark, tabId) => {
+  console.log(`Playing bookmark for video ${videoKey}: ${bookmark.desc}`);
+  const startPoint = getSecondsFromTime(bookmark.start);
+  const endPoint = getSecondsFromTime(bookmark.end);
+  const interval = (endPoint - startPoint) + 1;
+  // Construct the new YouTube video URL
+  const newUrl = `https://www.youtube.com/watch?v=${videoKey}&t=${startPoint}s`;
+  // Update the tab with the new URL
+  chrome.tabs.update({ url: newUrl });
+  timer = setTimeout(() => {
+    chrome.tabs.sendMessage(tabId, { type: "PAUSE_VIDEO" });
+  }, interval * 1000);
+};
+
+clearTimeout(timer);
+
+// Listener for tab updates
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  let timer;
   clearTimeout(timer);
-
   if (tab.url && tab.url.includes("youtube.com/watch")) {
-
-    const getSecondsFromTime = (timeString) => {
-      const timeParts = timeString.split(":");
-      const hours = parseInt(timeParts[0]);
-      const minutes = parseInt(timeParts[1]);
-      const seconds = parseInt(timeParts[2]);
-      
-      return hours * 3600 + minutes * 60 + seconds;
-    };
-
+    // Listener for messages from content scripts
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (message.type === "PLAY_BOOKMARK") {
         const { videoKey, bookmark } = message;
-        console.log(`Playing bookmark for video ${videoKey}: ${bookmark.desc}`);
-
-        const startPoint = getSecondsFromTime(bookmark.start);
-
-        const endPoint = getSecondsFromTime(bookmark.end);
-    
-        const interval = (endPoint - startPoint) + 1;
-        // Construct the new YouTube video URL
-        const newUrl = `https://www.youtube.com/watch?v=${videoKey}&t=${startPoint}s`;
-    
-        chrome.tabs.update({ url: newUrl });       
-        
-        clearTimeout(timer);
-
-        // After a certain duration (e.g., 60 seconds), send message to pause the video
-    timer = setTimeout(() => {
-      chrome.tabs.sendMessage(tabId, { type: "PAUSE_VIDEO" });
-    }, interval * 1000); 
-    
+        playBookmark(videoKey, bookmark, tabId);
       }
     });
-      
   }
 });
 
+// Listener for messages from popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "OPEN_POPUP") {
-
     chrome.windows.create({
       url: `window.html?currentVideo=${message.value}&videoStart=${message.start}&videoEnd=${message.end}`,
       type: "popup",
@@ -52,4 +49,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     });
   }
 });
-
